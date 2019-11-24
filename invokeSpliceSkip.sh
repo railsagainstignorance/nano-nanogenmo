0d2acbe0cd3c373ab6cbda31b25fc72650d2ba7c
Chris Gathercole <cgatherc@hotmail.com>
2019-11-24 22:52:24 +0000

    tweak to make novel nearer 50k words

--- invokeSpliceSkip.sh
+++ invokeSpliceSkip.sh
@@ -2,11 +2,11 @@
 
 # For spliceSkip algorithm.
 # Code was developed in spliceSkip.pl, then minified into s.pl
-# In the while loop, increase the completion threshold to approx 2500, while($c++<2500), to be sure to generate > 50K words.
+# In the while loop, increase the completion threshold to approx 500, while($c++<500), to be sure to generate > 50K words.
 # The argument is a text file named t.
 # Total chars = 256
 
-perl -e'sub f{$N=1+int(rand(5));if(shift=~/^(\W*(?:\w+\W*){${N}})(\w+)(\W+)(.*)/){$b=$1;if($4=~/.*?(\W+${2})(.*)/i){return$b.$1.f($2);}return$1.$2.$3;}}$_=join("",<>);s/\R/ /g;s/.*Chapter 1 (.+)/\1/;while($c++<2500){$T=f($_)=~s/\s+/ /gr;print"$T.\n\n";}' t
+perl -e'sub f{$N=1+int(rand(5));if(shift=~/^(\W*(?:\w+\W*){${N}})(\w+)(\W+)(.*)/){$b=$1;if($4=~/.*?(\W+${2})(.*)/i){return$b.$1.f($2);}return$1.$2.$3;}}$_=join("",<>);s/\R/ /g;s/.*Chapter 1 (.+)/\1/;while($c++<500){$T=f($_)=~s/\s+/ /gr;print"$T.\n\n";}' t
 
 # The basic algorithm is:
 # - skip over the intro blurb til the 2nd mention of "Chapter 1"
