be00b75675cc1ff7920380304d0a00e6b976874e
Chris Gathercole <chris.gathercole@ft.com>
2019-11-29 09:05:55 +0000

    first go at deconstructing yogi bera

--- invokeYogi.sh
+++ invokeYogi.sh
@@ -1,21 +1,18 @@
-#!/usr/bin/bash
-
 # For Permute algorithm.
 # Code was developed in permute.pl, then minified into p.pl
 # Total chars = 249
 # (depends if you count \n - if so, just remove them)
 # (The qw(...) thing saves a few chars if the list has more than 2 strings in it)
+# Yogi Berra quote via https://en.wikiquote.org/wiki/Yogi_Berra
+# "You can observe a lot by watching"
 
 perl -e'$r=[
-[qw(I We You)],
-[qw(ran hop go)],
-["to","by"],
-[qw(the a our)],
-[qw(red blue grey)],
-["egg","cup"],
-["of","for"],
-["my","your"],
-[qw(pal owl bud dog set)]
+[qw(You I We All)],
+[qw(can do did may)],
+[qw(observe see spot get gain spy)],
+["a lot","much","some","lots","nowt","all"],
+[qw(by with without in)],
+[qw(watching seeing listening doing)]
 ];sub p{my($s,$i)=@_;my$w=$r->[$i];if(!@$w){print"$s.\n";return}map{p("$s $_",$i+1)}@$w}p;'
 
 # The basic algorithm is:
