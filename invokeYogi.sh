ff8798dfe8b43e824eee81752217ba16aa570509
Chris Gathercole <chris.gathercole@ft.com>
2019-11-29 09:32:20 +0000

    squeezed into 256chars

--- invokeYogi.sh
+++ invokeYogi.sh
@@ -6,14 +6,7 @@
 # Yogi Berra quote via https://en.wikiquote.org/wiki/Yogi_Berra
 # "You can observe a lot by watching"
 
-perl -e'$r=[
-[qw(You I We All)],
-[qw(can do did may)],
-[qw(observe see spot get gain spy)],
-["a lot","much","some","lots","nowt","all"],
-[qw(by with without in)],
-[qw(watching seeing listening doing)]
-];sub p{my($s,$i)=@_;my$w=$r->[$i];if(!@$w){print"$s.\n";return}map{p("$s $_",$i+1)}@$w}p;'
+perl -e'@r=split(",","You_I_We_All,can_do_did_may,observe_see_spot_get_gain,a lot_much_some_lots_nowt_all_it,by_with_without_in,watching_seeing_listening_doing");sub p{my($s,$i)=@_;my@w=split("_",$r[$i]);if(!@w){print"$s.\n"}else{map{p("$s $_",$i+1)}@w}}p'
 
 # The basic algorithm is:
 # - iterate through all permutations of this sentence.
